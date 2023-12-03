bb3ff03f88645472d14b31d7cc83164fb3bbc7fa
Dima Zv <dimazverincevchanel@gmail.com>
2023-12-03 17:26:59 +0300

    change to queue in syntax

--- src/syntax.cpp
+++ src/syntax.cpp
@@ -29,12 +29,13 @@ static const char* word_to_str(Word word) {
 
 static std::string rule_to_string(Rule rule) {
 
-    if(rule.size()==0) return "{}";
+    if (rule.size()==0) return "{}";
 
-    std::stringstream ret;
+    std::stringstream ret; 
     ret << "{";
-    auto i=rule.size();
-    for (auto elem = rule.crbegin(); elem != rule.crend(); ++elem)  ret << word_to_str(*elem) << (--i==0? '}': ',');
+    ret << word_to_str(rule.front());
+    if (rule.size()>1) ret << ',' << word_to_str(rule.back());
+    ret << '}';
     return ret.str();
 }
 
@@ -64,7 +65,7 @@ static Word token_to_word(Token* token) {
 void Synt::read() {
 
     LOGI("\nSyntax analisys:\n")
-    rules.push({W_E});
+    rules.push(begin);
     Token* token;
     *input>>token;
 
@@ -87,8 +88,8 @@ bool Synt::check(Token* token) {
 
         rules.pop();
         LOGI("Rule: %s\tType: %s", rule_to_string(rule).c_str(), type_to_str(token->type))
-        Word word = rule.empty() ? W_UDEFINED : *rule.rbegin();
-        rule.pop_back();
+        Word word = rule.empty() ? W_UDEFINED : rule.front();
+        rule.pop();
 
         switch (word) {
             case W_E:
@@ -130,30 +131,45 @@ bool Synt::check(Token* token) {
     return false;
 }
 
-// Make table values vice versa for easy earasing elements
 void Synt::init_table() {
     // initialize rule set following table
 //               c  vecP    var   +   -   *   /   ;  (    )    =    
 //            E cP  vecP   varK      -E           ;  (E                                
 //            P                   +E -E   *E  /E  ;       )P      
 //            K                   +E -E   *E  /E  ;       )P   =E
-    table[E][W_CONST]           = {W_P, W_CONST};
-    table[E][W_VECTOR]          = {W_P, W_VECTOR};
-    table[E][W_VAR]             = {W_K, W_VAR};
-    table[E][W_END_EQUATION]    = {W_END_EQUATION};
-    table[P][W_END_EQUATION]    = {W_END_EQUATION};
-    table[E][W_MINUS]           = {W_E, W_MINUS};  
-    table[P][W_PLUS]            = {W_E, W_PLUS};
-    table[P][W_MINUS]           = {W_E, W_MINUS};
-    table[P][W_PRODUCT]         = {W_E, W_PRODUCT};
-    table[P][W_DIV]             = {W_E, W_DIV};
-    table[E][W_OPEN_PARENTH]    = {W_E, W_OPEN_PARENTH};
-    table[P][W_CLOSE_PARENTH]   = {W_P, W_CLOSE_PARENTH};
-    table[K][W_PLUS]            = {W_E, W_PLUS};
-    table[K][W_MINUS]           = {W_E, W_MINUS};
-    table[K][W_DIV]             = {W_E, W_DIV};
-    table[K][W_PRODUCT]         = {W_E, W_PRODUCT};
-    table[K][W_CLOSE_PARENTH]   = {W_P, W_CLOSE_PARENTH};
-    table[K][W_ASSIGN]          = {W_E, W_ASSIGN};
-    table[K][W_END_EQUATION]    = {W_END_EQUATION};
+    table[E][W_CONST]        .push(W_CONST);
+    table[E][W_CONST]        .push(W_P);
+    table[E][W_VECTOR]       .push(W_VECTOR);
+    table[E][W_VECTOR]       .push(W_P);
+    table[E][W_VAR]          .push(W_VAR);
+    table[E][W_VAR]          .push(W_K);   
+    table[E][W_END_EQUATION] .push(W_END_EQUATION);
+    table[P][W_END_EQUATION] .push(W_END_EQUATION);
+    table[E][W_MINUS]        .push(W_MINUS);
+    table[E][W_MINUS]        .push(W_E);  
+    table[P][W_PLUS]         .push(W_PLUS);
+    table[P][W_PLUS]         .push(W_E);
+    table[P][W_MINUS]        .push(W_MINUS);
+    table[P][W_MINUS]        .push(W_E);
+    table[P][W_PRODUCT]      .push(W_PRODUCT);
+    table[P][W_PRODUCT]      .push(W_E);
+    table[P][W_DIV]          .push(W_DIV);
+    table[P][W_DIV]          .push(W_E);
+    table[E][W_OPEN_PARENTH] .push(W_OPEN_PARENTH);
+    table[E][W_OPEN_PARENTH] .push(W_E);
+    table[P][W_CLOSE_PARENTH].push(W_CLOSE_PARENTH);
+    table[P][W_CLOSE_PARENTH].push(W_P);
+    table[K][W_PLUS]         .push(W_PLUS);
+    table[K][W_PLUS]         .push(W_E);   
+    table[K][W_MINUS]        .push(W_MINUS);
+    table[K][W_MINUS]        .push(W_E);
+    table[K][W_DIV]          .push(W_DIV);
+    table[K][W_DIV]          .push(W_E);
+    table[K][W_PRODUCT]      .push(W_PRODUCT);
+    table[K][W_PRODUCT]      .push(W_E);
+    table[K][W_CLOSE_PARENTH].push(W_CLOSE_PARENTH);
+    table[K][W_CLOSE_PARENTH].push(W_P);
+    table[K][W_ASSIGN]       .push(W_ASSIGN);
+    table[K][W_ASSIGN]       .push(W_E);
+    table[K][W_END_EQUATION] .push(W_END_EQUATION);
 }
--- src/syntax.h
+++ src/syntax.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <queue>
 #include <stack>
 #include <vector>
 #include "token.h"
@@ -15,17 +16,18 @@ class ParenthesisException: public std::exception {};
 enum Word{W_UDEFINED, W_CONST, W_VECTOR, W_VAR, W_END_EQUATION, W_PLUS, W_MINUS, W_PRODUCT, W_DIV,
           W_OPEN_PARENTH, W_CLOSE_PARENTH,  W_ASSIGN, W_E, W_P, W_K, COUNT};  
 
-typedef  std::vector<Word> Rule;
+typedef  std::queue<Word> Rule;
 
 class Synt {
 public:
-    Synt(Stream* stream): input{stream} { init_table(); }
+    Synt(Stream* stream): input{stream} { begin.push(W_E); init_table(); }
     ~Synt() {}
     void read();
 private:
     Stream* input {nullptr};
     Token* prev {nullptr};
     int cnt_op {0}, cnt_cp {0};         // count open close parenthesis
+    Rule begin;                         // Initial position of rule stack, must be W_E
     std::stack<Rule> rules;
     Rule table[3][Word::COUNT-3];  // rule table 3: W_E, W_P, W_K;
 
